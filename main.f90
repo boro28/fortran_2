6c5802f468bd9a0650b2fadfd4f21d2e6f828ca6
Szymon Borkowski <borkowskiszymon28@gmail.com>
2018-06-02 04:26:14 +0200

    exercise 3 done

---
+++ main.f90
@@ -0,0 +1,86 @@
+subroutine start_clock(Icount)
+    integer(kind = 4), intent(inout) :: Icount
+    integer(kind = 4) :: ir, im
+
+    call system_clock(Icount, ir, im)
+
+end subroutine start_clock
+
+
+subroutine stop_clock(Dtime, Icount)
+    integer(kind = 4), intent(in) :: Icount
+    real(kind = 8), intent(out) :: Dtime
+    integer(kind = 4) :: ic, ir, im
+
+    call system_clock(ic, ir, im)
+    ! write(*, *) 'start time ', Icount, ' stop time', ic
+
+    if (ic > Icount) then
+        Dtime = dble(ic - Icount) / dble(ir)
+    elseif (ic < Icount) then
+        Dtime = dble(im + ic - Icount) / dble(ir)
+    else
+        Dtime = 0.d0
+    endif
+
+end subroutine stop_clock
+
+program fortra_2project! pierwsza macierz
+    use matrix
+    implicit none
+    real (kind = 8), allocatable :: first(:, :) ! pierwsza macierz
+    real (kind = 8), allocatable :: second(:, :) ! druga macierz
+    real (kind = 8), allocatable :: multiply(:, :) ! macierz wynikowa
+    integer (kind = 4) :: status ! kod błędu, 0 gdy OK
+    integer(kind = 4) :: iclock, i
+    integer(kind = 4), dimension(3) :: AllocateStatus, DeAllocateStatus
+    real (kind = 8), dimension(5) :: dtime
+
+    open(unit = 2, file = "results.txt")
+
+    do i = 10, 1000, 10
+        allocate(first(i, i), STAT = AllocateStatus(1))
+        allocate(second(i, i), STAT = AllocateStatus(2))
+        allocate(multiply(i, i), STAT = AllocateStatus(3))
+        if(MAXVAL(AllocateStatus) .EQ. 1) then
+            write(2, *)"Error allocating memory"
+            STOP
+        end if
+
+        first = 2
+        second = 3
+
+        call start_clock(iclock) !starts the clock
+        call mm(first, second, multiply, status)
+        call stop_clock(dtime(1), iclock) !end the clock
+
+        call start_clock(iclock) !starts the clock
+        call mm_dot(first, second, multiply, status)
+        call stop_clock(dtime(2), iclock) !end the clock
+
+        call start_clock(iclock) !starts the clock
+        call mm_chunk(first, second, multiply, status)
+        call stop_clock(dtime(3), iclock) !end the clock
+
+        call start_clock(iclock) !starts the clock
+        call mm_dot_and_chunk(first, second, multiply, status)
+        call stop_clock(dtime(4), iclock) !end the clock
+
+        call start_clock(iclock) !starts the clock
+        multiply = matmul(first, second)
+        call stop_clock(dtime(5), iclock) !end the clock
+
+        write(2, *) "mm:", dtime(1), "dot:", dtime(2), "chunk:", dtime(3), "dot_and_chunk:", dtime(4), "matmul:", dtime(5)
+
+        deallocate(first, STAT = DeAllocateStatus(1))
+        deallocate(second, STAT = DeAllocateStatus(2))
+        deallocate(multiply, STAT = DeAllocateStatus(3))
+
+        if(MAXVAL(AllocateStatus) .EQ. 1) then
+            write(2, *)"Error deallocating memory"
+            STOP
+        end if
+
+    end do
+
+end program
--- matrix.f90
+++ matrix.f90
@@ -3,30 +3,30 @@ module matrix
 contains
     subroutine mm(first, second, multiply, status)
         implicit none
-        real (kind = 8), intent(in) :: first(:,:) ! pierwsza macierz
-        real (kind = 8), intent(in) :: second(:,:) ! druga macierz
-        real (kind = 8), intent(out) :: multiply(:,:) ! macierz wynikowa
+        real (kind = 8), intent(in) :: first(:, :) ! pierwsza macierz
+        real (kind = 8), intent(in) :: second(:, :) ! druga macierz
+        real (kind = 8), intent(out) :: multiply(:, :) ! macierz wynikowa
         integer (kind = 4), intent(out) :: status ! kod błędu, 0 gdy OK
 
-        integer (kind = 4), dimension(2) :: f_s, s_s,m_s
-        integer (kind = 4) :: i,j,k
+        integer (kind = 4), dimension(2) :: f_s, s_s, m_s
+        integer (kind = 4) :: i, j, k
         real (kind = 8) :: tmp
 
         status = 1  !if multipling loop doesn't end status will be 1 for failed multiplication
 
         !gets sizes of given matrixes
-        f_s=shape(first)
-        s_s=shape(second)
-        m_s=shape(multiply)
+        f_s = shape(first)
+        s_s = shape(second)
+        m_s = shape(multiply)
 
-        if ( (f_s(2) .EQ. s_s(1)) .AND. (f_s(1) .EQ. m_s(1)) .AND. (s_s(2) .EQ. m_s(2)) ) then
-            do i=1,m_s(1)           !columns
-                do j=1,m_s(2)       !rows
-                    tmp=0.d0
-                    do k=1,f_s(2)
-                        tmp = tmp + first(i,k)*second(k,j)
+        if ((f_s(2) .EQ. s_s(1)) .AND. (f_s(1) .EQ. m_s(1)) .AND. (s_s(2) .EQ. m_s(2))) then
+            do i = 1, m_s(1)           !columns
+                do j = 1, m_s(2)       !rows
+                    tmp = 0.d0
+                    do k = 1, f_s(2)
+                        tmp = tmp + first(i, k) * second(k, j)
                     end do
-                    multiply(i,j)=tmp
+                    multiply(i, j) = tmp
                 end do
             end do
             status = 0               !if all goes as planned status is set to 0
@@ -36,25 +36,25 @@ contains
 
     subroutine mm_dot(first, second, multiply, status)
         implicit none
-        real (kind = 8), intent(in) :: first(:,:) ! pierwsza macierz
-        real (kind = 8), intent(in) :: second(:,:) ! druga macierz
-        real (kind = 8), intent(out) :: multiply(:,:) ! macierz wynikowa
+        real (kind = 8), intent(in) :: first(:, :) ! pierwsza macierz
+        real (kind = 8), intent(in) :: second(:, :) ! druga macierz
+        real (kind = 8), intent(out) :: multiply(:, :) ! macierz wynikowa
         integer (kind = 4), intent(out) :: status ! kod błędu, 0 gdy OK
 
-        integer (kind = 4), dimension(2) :: f_s, s_s,m_s
-        integer (kind = 4) :: i,j
+        integer (kind = 4), dimension(2) :: f_s, s_s, m_s
+        integer (kind = 4) :: i, j
 
         status = 1  !if multipling loop doesn't end status will be 1 for failed multiplication
 
         !gets sizes of given matrixes
-        f_s=shape(first)
-        s_s=shape(second)
-        m_s=shape(multiply)
+        f_s = shape(first)
+        s_s = shape(second)
+        m_s = shape(multiply)
 
-        if ( (f_s(2) .EQ. s_s(1)) .AND. (f_s(1) .EQ. m_s(1)) .AND. (s_s(2) .EQ. m_s(2)) ) then
-            do i=1,m_s(1) !columns
-                do j=1,m_s(2) !rows
-                    multiply(i, j) = dot_product(first(i,:),second(:,j))
+        if ((f_s(2) .EQ. s_s(1)) .AND. (f_s(1) .EQ. m_s(1)) .AND. (s_s(2) .EQ. m_s(2))) then
+            do i = 1, m_s(1) !columns
+                do j = 1, m_s(2) !rows
+                    multiply(i, j) = dot_product(first(i, :), second(:, j))
                 end do
             end do
             status = 0  !if all goes as planned status is set to 0
@@ -64,23 +64,23 @@ contains
 
     subroutine mm_chunk(first, second, multiply, status)
         implicit none
-        real (kind = 8), intent(in) :: first(:,:) ! pierwsza macierz
-        real (kind = 8), intent(in) :: second(:,:) ! druga macierz
-        real (kind = 8), intent(out) :: multiply(:,:) ! macierz wynikowa
+        real (kind = 8), intent(in) :: first(:, :) ! pierwsza macierz
+        real (kind = 8), intent(in) :: second(:, :) ! druga macierz
+        real (kind = 8), intent(out) :: multiply(:, :) ! macierz wynikowa
         integer (kind = 4), intent(out) :: status ! kod błędu, 0 gdy OK
 
-        integer (kind = 4), dimension(2) :: f_s, s_s,m_s
-        integer (kind = 4) :: i,j,k,jj,kk,ichunk
+        integer (kind = 4), dimension(2) :: f_s, s_s, m_s
+        integer (kind = 4) :: i, j, k, jj, kk, ichunk
         real (kind = 8) :: tmp
 
         status = 1  !if multipling loop doesn't end status will be 1 for failed multiplication
 
         !gets sizes of given matrixes
-        f_s=shape(first)
-        s_s=shape(second)
-        m_s=shape(multiply)
+        f_s = shape(first)
+        s_s = shape(second)
+        m_s = shape(multiply)
 
-        if ( (f_s(2) .EQ. s_s(1)) .AND. (f_s(1) .EQ. m_s(1)) .AND. (s_s(2) .EQ. m_s(2)) ) then
+        if ((f_s(2) .EQ. s_s(1)) .AND. (f_s(1) .EQ. m_s(1)) .AND. (s_s(2) .EQ. m_s(2))) then
             ichunk = 512 ! I have a 3MB cache size (real*4)
             do jj = 1, m_s(1), ichunk
                 do kk = 1, m_s(2), ichunk
@@ -103,28 +103,28 @@ contains
 
     subroutine mm_dot_and_chunk(first, second, multiply, status)
         implicit none
-        real (kind = 8), intent(in) :: first(:,:) ! pierwsza macierz
-        real (kind = 8), intent(in) :: second(:,:) ! druga macierz
-        real (kind = 8), intent(out) :: multiply(:,:) ! macierz wynikowa
+        real (kind = 8), intent(in) :: first(:, :) ! pierwsza macierz
+        real (kind = 8), intent(in) :: second(:, :) ! druga macierz
+        real (kind = 8), intent(out) :: multiply(:, :) ! macierz wynikowa
         integer (kind = 4), intent(out) :: status ! kod błędu, 0 gdy OK
 
-        integer (kind = 4), dimension(2) :: f_s, s_s,m_s
-        integer (kind = 4) :: j,k,jj,kk,ichunk
+        integer (kind = 4), dimension(2) :: f_s, s_s, m_s
+        integer (kind = 4) :: j, k, jj, kk, ichunk
 
         status = 1  !if multipling loop doesn't end status will be 1 for failed multiplication
 
         !gets sizes of given matrixes
-        f_s=shape(first)
-        s_s=shape(second)
-        m_s=shape(multiply)
+        f_s = shape(first)
+        s_s = shape(second)
+        m_s = shape(multiply)
 
-        if ( (f_s(2) .EQ. s_s(1)) .AND. (f_s(1) .EQ. m_s(1)) .AND. (s_s(2) .EQ. m_s(2)) ) then
+        if ((f_s(2) .EQ. s_s(1)) .AND. (f_s(1) .EQ. m_s(1)) .AND. (s_s(2) .EQ. m_s(2))) then
             ichunk = 512 ! I have a 3MB cache size (real*4)
             do jj = 1, m_s(1), ichunk
                 do kk = 1, m_s(2), ichunk
                     do j = jj, min(jj + ichunk - 1, m_s(1))
                         do k = kk, min(kk + ichunk - 1, m_s(2))
-                            multiply(j, k) = dot_product(first(j,:),second(:,k))
+                            multiply(j, k) = dot_product(first(j, :), second(:, k))
                         end do
                     end do
 
